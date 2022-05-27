20a73363770607fc58db9d5097f19ca6764f788a
admin <anaspiko2@gmail.com>
2022-05-27 11:47:29 +0200

    initial commit

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,43 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on,
+  State
+} from '@ngrx/store';
+import { environment } from '../../../environments/environment';
+import { AuthActions } from '../login/action-types';
+import { User } from '../model/user.model';
+
+export const authFeatureKey = 'auth';
+
+export interface AuthState {
+    user: User
+}
+
+export const initialAuthState: AuthState = {
+    user: undefined
+}
+
+export const authReducer = createReducer(
+    initialAuthState,
+
+    on(AuthActions.login, (state, action) => {
+
+      return {
+        user: action.user
+      }
+    }),
+
+    on(AuthActions.logout, (state, action) => {
+      return {
+        user: undefined
+      }
+    })
+
+
+);
+
